96cefcd852f5eb806848f05ef8df3e34e607138b
Christopher Harding <chrisharding88@gmail.com>
2019-08-27 02:24:15 -0400

    working on the user values on app.js

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
 var winCounter = 0;
 var lossCounter = 0;
 var gemOptions = [];
+var gems;
+var gemValue;
 var randomNum = 0;
 var userNum = 0
 
@@ -12,10 +14,39 @@ $(document).ready(function(){
  // Prints out the random number
  $("#randomNum").html(randomNum);
 
+ // Chooses each gem between 1 - 12
+ gemOptions = [Math.floor(Math.random() * 12) + 1]; 
+
+for (var i = 0; i < gemOptions.length; i++){
+
+gems = $('.jewels');
+gems.attr("data-gemvalue", gemOptions[i]);
+$('.gems').append(gems);
+
+}
 
 
+$('.jewels').on('click', function(){
+
+gemValue = ($(this).attr('data-gemvalue'));
+gemValue = parseInt(gemValue);
+
+userNum += gemValue;
+
+$('#userNum').html(userNum);
+
+if (userNum === randomNum){
+    alert("You win");
+    winCounter++
+    $('#winCount').html(winCounter);
+} else if (userNum >= randomNum){
+    alert("You Lose");
+    lossCounter++;
+    $('#lossCount').html(lossCounter);
+}
 
 
+});
 
 
 
