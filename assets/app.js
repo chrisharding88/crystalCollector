070e9a4bc80319809d74c1d801ab5f9ecacbdbe3
Christopher Harding <chrisharding88@gmail.com>
2019-08-27 01:06:39 -0400

    prints out random number between 19-120

---
+++ assets/app.js
@@ -0,0 +1,66 @@
+var winCounter = 0;
+var lossCounter = 0;
+var gemOptions = [];
+var randomNum = 0;
+var userNum = 0
+
+ 
+$(document).ready(function(){
+
+ // Chooses the random number between 19 - 120   
+ randomNum = [Math.floor(Math.random() * 120) + 19]; 
+ // Prints out the random number
+ $("#randomNum").html(randomNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
