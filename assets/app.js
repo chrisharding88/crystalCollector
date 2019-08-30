fe9860fd5dc89705ae63e37163758b440bb16020
Christopher Harding <chrisharding88@gmail.com>
2019-08-29 21:44:36 -0400

    Set up the night mode and set it back to default using one button

--- assets/app.js
+++ assets/app.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
     var winCounter = 0;
     var lossCounter = 0;
     var gemOptions = [];
-    var circle = $('#circle');
-    var roundSquare = $('#roundSquare');
-    var rectangle = $('#rectangle');
-    var triangle = $('#triangle');
+    var circle;
+    var roundSquare;
+    var rectangle;
+    var triangle;
     var gem1;
     var gem2;
     var gem3;
@@ -15,6 +15,8 @@ $(document).ready(function() {
     var gemValue;
     var randomNum = 0;
     var userNum = 0
+
+    var isDarkMode = false;
     
 
  // Chooses the random number between 19 - 120   
@@ -23,7 +25,7 @@ $(document).ready(function() {
  $("#randomNum").html(randomNum);
 
 
- // Chooses each gem between 1 - 12
+ // Each gem will set the number between 1 - 12
  circle = Math.floor(Math.random() * 12) + 1; 
  roundSquare = Math.floor(Math.random() * 12) + 1; 
  rectangle = Math.floor(Math.random() * 12) + 1; 
@@ -59,9 +61,40 @@ for (var i = 0; i < gemOptions.length; i++){
 
 function reset(){
     randomNum = Math.floor(Math.random() * 120) + 19; 
+    circle = Math.floor(Math.random() * 12) + 1; 
+    roundSquare = Math.floor(Math.random() * 12) + 1; 
+    rectangle = Math.floor(Math.random() * 12) + 1; 
+    triangle = Math.floor(Math.random() * 12) + 1; 
+    gemOptions.push(circle, roundSquare, rectangle, triangle);
+
+    for (var i = 0; i < gemOptions.length; i++){
+        if (i === 0){
+        gem1 = $('#circle');
+        gem1.attr('data-crystalvalue', gemOptions[i]);
+        } else if (i === 1){
+        gem2 = $('#roundSquare');
+        gem2.attr('data-crystalvalue', gemOptions[i]);
+        } else if (i === 2){
+        gem3 = $('#rectangle');
+        gem3.attr('data-crystalvalue', gemOptions[i]);
+        } else if (i === 3){
+        gem4 = $('#triangle');
+        gem4.attr('data-crystalvalue', gemOptions[i]);
+        }  
+    } 
+    circle;
+    roundSquare;
+    rectangle;
+    triangle;
+     gem1;
+     gem2;
+     gem3;
+     gem4;
     userNum = 0;
     gemOptions = [];
     $("#randomNum").html(randomNum);
+    $("#userNum").html(userNum);
+
 
 }
 
@@ -99,11 +132,19 @@ $('.jewels').on('click', function(){
 
 // Changes to Night Mode after the button is clicked
 $('button').on('click', function(){
-    $('body').css({'background-color':'black'});
-    $('header').css({'border-bottom-style' : 'solid' , 'border-bottom-color': 'white', 'border-border-width': '2px'});
-    $('.winLoseBoard').css({'color': 'black'});
-    $('.scoreboard').css({'color': 'black'});
-    
+    if (isDarkMode){
+        $('body').css({'background-color':'#04c4c1'});
+        $('header').css({'border-bottom-style' : 'solid' , 'border-bottom-color': 'white', 'border-border-width': '2px'});
+        $('.winLoseBoard').css({'color': '#04c4c1'});
+        $('.scoreboard').css({'color': '#04c4c1'});
+        isDarkMode =false;
+    } else {
+        $('body').css({'background-color':'black'});
+        $('header').css({'border-bottom-style' : 'solid' , 'border-bottom-color': 'white', 'border-border-width': '2px'});
+        $('.winLoseBoard').css({'color': 'black'});
+        $('.scoreboard').css({'color': 'black'});
+        isDarkMode = true;
+    }
 });
 
 
